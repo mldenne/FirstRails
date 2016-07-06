20afd0760b1c71128f723d820ad73b29730ccb77
Marie Denney <mldenne@gmail.com>
2016-07-05 20:25:44 -0400

    Final commit

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,10 +10,14 @@ class WelcomeController < ApplicationController
 
   def lorem
     if params[:id] == "standard"
-      render text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      standard
     else
       render text: "Wrong lorem selected."
     end
+  end
+
+  def standard
+    render text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   end
 
 end
